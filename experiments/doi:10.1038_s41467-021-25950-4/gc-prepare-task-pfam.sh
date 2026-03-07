70d7a68c41645ff2b0b6fefe37c357c7901d6624
Benjie Chen <benjie@alum.mit.edu>
2026-03-07 08:34:13 -0500

    updated Pfam job description for running on cheaper hardware

--- experiments/doi:10.1038_s41467-021-25950-4/gc-prepare-task-pfam.sh
+++ experiments/doi:10.1038_s41467-021-25950-4/gc-prepare-task-pfam.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e  # Exit immediately if any command fails
 
-MOUNT_PATH="/mnt/disks/ssd"
+WORKSPACE="/data"
 
 # Clean up any previous task inputs and outputs
-rm -f $MOUNT_PATH/input_*.faa
-rm -f $MOUNT_PATH/output_*.tsv
+rm -f $WORKSPACE/input_*.faa
+rm -f $WORKSPACE/output_*.tsv
 
 echo "Downloading input FASTA for task ${BATCH_TASK_INDEX}..."
 gsutil -o "GSUtil:check_hashes=never" cp \
   "gs://needle-files/experiments/doi:10.1038_s41467-021-25950-4/proteins_${BATCH_TASK_INDEX}.faa" \
-  "$MOUNT_PATH/input_${BATCH_TASK_INDEX}.faa"
+  "$WORKSPACE/input_${BATCH_TASK_INDEX}.faa"
