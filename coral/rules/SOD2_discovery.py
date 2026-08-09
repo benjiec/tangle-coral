ec6e3820f15131912ff27745d2b264f4f8808862
Benjie Chen <benjie@alum.mit.edu>
2026-08-09 16:05:33 -0400

    all k04564 matches against mature SOD2 HMM profiles

--- coral/rules/SOD2_discovery.py
+++ coral/rules/SOD2_discovery.py
@@ -17,3 +17,19 @@ rule = Rules(
     # proton gate-keeper
     & HMMAlignment("k04564.hmm").matches_regex("[QDK]", 196)
 )
+
+no_leader_rule = Rules(
+    # required domains
+      Pfam.matches("PF00081")
+    & Pfam.matches("PF02777")
+    & KO.matches("K04564")
+
+    # required catalytic residues to form metal binding pocket
+    & HMMAlignment("k04564.hmm").is_at("H", 61)
+    & HMMAlignment("k04564.hmm").is_at("H", 106)
+    & HMMAlignment("k04564.hmm").is_at("D", 187)
+    & HMMAlignment("k04564.hmm").is_at("H", 191)
+
+    # proton gate-keeper
+    & HMMAlignment("k04564.hmm").matches_regex("[QDK]", 196)
+)
