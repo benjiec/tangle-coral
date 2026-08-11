8656439d35965672626bb65abf2c4919b0a65e00
Benjie Chen <benjie@alum.mit.edu>
2026-08-11 13:46:33 -0400

    new profiles

--- coral/rules/sp_MnFeSOD_cnidaria.py
+++ coral/rules/sp_MnFeSOD_cnidaria.py
@@ -1,7 +1,7 @@
 from sieve.rules import HMMAlignment, KO, Leader, Pfam, Rules, TFMotifs
 
-rule_tangle_curated = Rules(
-    Leader().upstreamOfPfam("PF00081").betweenAA(-35,-15).is_SP(deeploc=True)
+rule = Rules(
+    Leader().upstreamOfPfam("PF00081").betweenAA(-40,-15).is_SP(deeploc=True)
 
     # required domains
     & Pfam.matches("PF00081")
@@ -24,22 +24,4 @@ rule_tangle_curated = Rules(
         "GM.5.0.bZIP",
         min_score_threshold=8,
     )
-)
-
-rule_fasta = Rules(
-    Leader().upstreamOfPfam("PF00081").betweenAA(-35,-15).is_SP()
-
-    # required domains
-    & Pfam.matches("PF00081")
-    & Pfam.matches("PF02777")
-    & KO.matches("K04564")
-
-    # required catalytic residues to form metal binding pocket
-    & HMMAlignment("k04564.hmm").is_at("H", 61)
-    & HMMAlignment("k04564.hmm").is_at("H", 106)
-    & HMMAlignment("k04564.hmm").is_at("D", 187)
-    & HMMAlignment("k04564.hmm").is_at("H", 191)
-
-    # proton gate-keeper
-    & HMMAlignment("k04564.hmm").matches_regex("[QDK]", 196)
 )
