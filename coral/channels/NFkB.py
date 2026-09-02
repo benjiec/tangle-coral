571a14f60996166ebbaa69d93fd2f14556815a51
Benjie Chen <benjie@alum.mit.edu>
2026-09-02 15:09:18 -0400

    channel rules for NFkB family proteins

---
+++ coral/channels/NFkB.py
@@ -0,0 +1,35 @@
+from sieve.channel_set import ChannelSet
+from sieve.channels import CompositionBiasChannel
+from sieve.channels import ShortMotifChannel
+from sieve.channels import NetChargeChannel
+from sieve.channels import HydropathyChannel
+from sieve.channels import HydrophobicMomentChannel
+from sieve.channels import DisorderPropensityChannel
+from sieve.channels import SequenceEntropyChannel
+from sieve.channels import DipeptideFrequencyChannel
+
+channels = ChannelSet([
+    # GRR
+    CompositionBiasChannel("GRR_comp", radius=16, residues="G"),
+    HydropathyChannel("GRR_hydropathy", radius=16),
+    SequenceEntropyChannel("GRR_entropy", radius=16),
+    DisorderPropensityChannel("GRR_disorder", radius=16),
+
+    # PEST
+    CompositionBiasChannel("PEST_comp", radius=9, residues="PEDST"),
+    NetChargeChannel("PEST_netcharge", radius=9),
+    HydropathyChannel("PEST_hydropathy", radius=9),
+    DisorderPropensityChannel("PEST_disorder", radius=9),
+
+    # TAD
+    CompositionBiasChannel("TAD_comp", radius=8, residues="FLWV"),
+    NetChargeChannel("TAD_netcharge", radius=8),
+    HydropathyChannel("TAD_hydropathy", radius=8),
+    HydrophobicMomentChannel("TAD_hydrophobic_moment", radius=6),
+
+    # NHR
+    DipeptideFrequencyChannel("sp_dipeptide", radius=18, dipeptide="SP"),
+    ShortMotifChannel("NHR_motif", r"[PV].I.[IT][FD]"),
+    HydropathyChannel("NHR_hydropathy", radius=18),
+    DisorderPropensityChannel("NHR_disorder", radius=18),
+])
