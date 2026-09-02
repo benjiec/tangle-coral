1308f74f0d2b23d77f12216634568f824beb9593
Benjie Chen <benjie@alum.mit.edu>
2026-09-02 16:55:03 -0400

    adjust channels

--- coral/channels/NFkB.py
+++ coral/channels/NFkB.py
@@ -22,7 +22,8 @@ channels = ChannelSet([
     DisorderPropensityChannel("PEST_disorder", radius=9),
 
     # TAD
-    CompositionBiasChannel("TAD_comp", radius=8, residues="FLWV"),
+    CompositionBiasChannel("TAD_comp", radius=8, residues="FLWVDE"),
+    ShortMotifChannel("TAD_motif", r"[FDAWV]..[ILVWY][FDAWV]..[FDAWV]"),
     NetChargeChannel("TAD_netcharge", radius=8),
     HydropathyChannel("TAD_hydropathy", radius=8),
     HydrophobicMomentChannel("TAD_hydrophobic_moment", radius=6),
