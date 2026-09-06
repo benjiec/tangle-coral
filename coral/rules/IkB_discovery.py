1bb14d188d95edb3386845bddf1e07d6f705fae1
Benjie Chen <benjie@alum.mit.edu>
2026-09-06 08:34:55 -0400

    more ikb stuff

--- coral/rules/IkB_discovery.py
+++ coral/rules/IkB_discovery.py
@@ -1,6 +1,18 @@
 from sieve.rules import HMMAlignment, Leader, Pfam, Rules, Sequence
 
-ikb_rule = Rules(
+βTrCP_pattern = r"[DE][ST]G[LIVMFYWA].{1,2}[ST]"
+
+inhibitor_rule = Rules(
+    # anything that matches Ankyrin repeat (3 copies)
+    Pfam.matches("PF12796")
+
+    # a few known negatives
+    & ~Pfam.matches("PF00554")
+    & ~Pfam.matches("PF08424")
+    & ~Pfam.matches("PF01876")
+)
+
+canonical_ikb_rule = Rules(
     # anything that matches Ankyrin repeat (3 copies)
     Pfam.matches("PF12796")
     & Sequence.length_between(300, 1200)
@@ -11,21 +23,17 @@ ikb_rule = Rules(
     & ~Pfam.matches("PF01876")
 
     # required IKK phosphorylation and then degradation by βTrCP
-    & Sequence.matches_regex(r"[DE][ST]G[LIVMFYWA].{1,2}[ST]").relativeToPfam("PF12796", -100, -20)
+    & Sequence.matches_regex(βTrCP_pattern).relativeToPfam("PF12796", -100, -20)
 )
 
-bcl3_rule = Rules(
+nucleus_inhinitor_rule = Rules(
     # anything that matches Ankyrin repeat (3 copies)
     Pfam.matches("PF12796")
-    & Sequence.length_between(300, 1200)
 
     # a few known negatives
     & ~Pfam.matches("PF00554")
     & ~Pfam.matches("PF08424")
     & ~Pfam.matches("PF01876")
-
-    # does not have a DSG..S site
-    & ~Sequence.matches_regex(r"[DE][ST]G[LIVMFYWA].{1,2}[ST]").relativeToPfam("PF12796", -100, -20)
 
     # NLS
     & (Sequence.matches_regex(
@@ -35,3 +43,5 @@ bcl3_rule = Rules(
          r"[KR]{2}.{10,14}[KR]{3,5}"
        ).relativeToPfam("PF12796", -90, -20))
 )
+
+# also BCL3 does not have DSG..S
