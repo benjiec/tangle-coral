58d11314cf0e6ef876da6f89b359d972838cb138
Benjie Chen <benjie@alum.mit.edu>
2026-09-07 09:55:19 -0400

    updated rules

--- coral/rules/IkB_discovery.py
+++ coral/rules/IkB_discovery.py
@@ -1,30 +1,59 @@
 from sieve.rules import HMMAlignment, Leader, Pfam, Rules, Sequence
 
-βTrCP_pattern = r"[DE][ST]G[LIVMFYWA].{1,2}[ST]"
+# lysine (K) within 40 AAs upstream of a DSG(\phi).S degron motif
+βTrCP_pattern = r"K.{1,40}?[DEST][ST]G[LIVMFYWA].{1,2}[ST]"
 
-ank_rule = Rules(
-    # anything that matches Ankyrin repeat (3 copies)
-    Pfam.matches("PF12796")
 
-    # a few known negatives
-    & ~Pfam.matches("PF00554")
-    & ~Pfam.matches("PF08424")
-    & ~Pfam.matches("PF01876")
+# all encompassing rule to filter sequences so we can send them to DeepLoc
+no_deeploc_rule = Rules(
+
+    Pfam.matches("PF00023").times(3, 11)
+    & Sequence.length_between(300, 1000)
+    & Pfam.matches_only("PF00023", "PF12796", "PF13606", "PF13637", "PF13857")
+
+    & Pfam.matches("PF00023").betweenAA(30, 800, all_matches=True)
 )
+
 
 canonical_ikb_rule = Rules(
-    # anything that matches Ankyrin repeat (3 copies)
-    Pfam.matches("PF12796")
-    & Sequence.length_between(300, 1200)
+    Leader().localize_at("Cytoplasm")
 
-    # a few known negatives
-    & ~Pfam.matches("PF00554")
-    & ~Pfam.matches("PF08424")
-    & ~Pfam.matches("PF01876")
+    & Pfam.matches("PF00023").times(3, 11)
+    & Sequence.length_between(300, 900)
+    & Pfam.matches_only("PF00023", "PF12796", "PF13606", "PF13637", "PF13857")
+   
+    # Canonical IκBs require an intact N-terminal Signal-Receiving Domain (SRD)
+    # ahead of Ankyrin Repeat 1. Minimum Leader Length: The SRD needs at least
+    # 30 to 70 amino acids to house the target IKK phosphorylation serines and
+    # lysine acceptor residues for ubiquitin attachment.
+    #
+    # Also want to limit the last match to be between 80 AAs from end of
+    # protein, to allow for PEST.
+    #
+    & Pfam.matches("PF00023").betweenAA(30, 800, all_matches=True)
 
     # required IKK phosphorylation and then degradation by βTrCP
-    & Sequence.matches_regex(βTrCP_pattern).relativeToPfam("PF12796", -180, -10)
+    & Sequence.matches_regex(βTrCP_pattern).relativeToPfam("PF00023", -200, -10)
 )
 
-# BCL3 requires localization=nucleus by DeepLoc
-# also BCL3 does not have DSG..S
+canonical_bcl3_rule = Rules(
+    Leader().localize_at("Nucleus")
+
+    & Pfam.matches("PF00023").times(3, 11)
+    & Sequence.length_between(300, 1000)
+    & Pfam.matches_only("PF00023", "PF12796", "PF13606", "PF13637", "PF13857")
+
+    # BCL-3 features an N-terminal leader of ~118 amino acids preceding Ankyrin
+    # Repeat 1 (which begins at residue ~119 in human BCL-3). This region
+    # contains its basic Nuclear Localization Signal (NLS) and
+    # proline/serine-rich regulation sites. Conservating setting start of ANK
+    # at 80.
+    #
+    # Also want to limit the last match to be between 80 to 200 AAs from end of
+    # protein, to allow for a TAD.
+    #
+    & Pfam.matches("PF00023").betweenAA(80, 800, all_matches=True)
+
+    # Does not have a DSG..S site
+    & ~Sequence.matches_regex(βTrCP_pattern).relativeToPfam("PF00023", -200, -10)
+)
