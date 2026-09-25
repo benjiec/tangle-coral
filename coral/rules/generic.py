b7392e920952f06b281f17b3a3545716966549b2
Benjie Chen <benjie@alum.mit.edu>
2026-09-25 14:15:47 -0400

    updated workbook

--- coral/rules/generic.py
+++ coral/rules/generic.py
@@ -1,5 +1,9 @@
-from sieve.rules import Rules, Sequence
+from sieve.rules import Rules, Sequence, TFMotifs
 
 nothing = Rules(
     Sequence.length_between(1, 1000000)
+)
+
+loci = Rules(
+    TFMotifs.has("something")
 )
